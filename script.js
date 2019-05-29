fc37b0a3c214310b1a170abb7b66b22ba850d3ee
damian0s10 <damian.wasiluk10@gmail.com>
2019-05-29 20:52:06 +0200

    second commit

--- script.js
+++ script.js
@@ -6,10 +6,12 @@ const counts = document.querySelectorAll('.count');
 let imgs = {};
 let inputValue;
 let numberImages = 0;
+let nextStart;
 
 function removeData(){
     containerImg.innerHTML = '';
 }
+
 function downloadData(e) {
 e.preventDefault();
 removeData();
@@ -20,47 +22,84 @@ fetch(`https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key
     .then(resp => {
         console.log(resp);
         imgs = resp.photos.photo; 
-        insertImages(numberImages);
+        insertImages(0, numberImages);
     });
 }
 else {
     alert('Select the number of photos');
 }}
+searchButton.addEventListener('click',downloadData);
+
+/*
+function removeLastNthChild(n) {
+    let NumberOfChildren= containerImg.children.length;
+    for(let i=(NumberOfChildren-1); i>=(NumberOfChildren-n); i--) {
+        containerImg.children[i].remove();
+        console.log('NumberOfChildren');
+    }
+} */
 
 function numberOfImages() {
     counts.forEach(count => count.classList.remove('active'));
     this.classList.add('active');
     numberImages = this.dataset.value;
+    if(inputValue && ((nextStart < parseInt(numberImages)) || numberImages ==='All')) {
+        insertImages(nextStart, numberImages);
+    }
+    
+    
+ /*   else if (inputValue && (nextStart > parseInt(numberImages))) {
+        let number = Math.abs(parseInt(numberImages)-nextStart);
+        console.log(number);
+        removeLastNthChild(number);
+        
+    }*/
 }
+counts.forEach(count => count.addEventListener('click',numberOfImages));
 
-function insertImages(x) {
-    const div = document.createElement('div');
-    div.classList.add('images');
-    if(x === 'All'){
-        for(let i=0; i<imgs.length; i++) {
+
+
+function insertImages(xStart, xEnd) {
+    
+    if(imgs.length === 0) {
+        const p = document.createElement('p');
+        p.innerText = `${inputValue} was not found`;
+        containerImg.appendChild(p);
+    }
+    else if(xEnd === 'All' || imgs.length < parseInt(xEnd)){
+        for(let i = xStart; i<imgs.length; i++) {
             let img = imgs[i];
             const divImg = document.createElement('div');
             divImg.classList.add('image');
             const picture = document.createElement('img');
             picture.src = `https://farm${img.farm}.staticflickr.com/${img.server}/${img.id}_${img.secret}.jpg`;  
             divImg.appendChild(picture);
-            div.appendChild(divImg);
+            const link = document.createElement('a');
+            link.href = picture.src;
+            link.target = '_blank';
+            link.appendChild(divImg);
+            containerImg.appendChild(link);
+            nextStart = parseInt(xEnd);
             }
     }
     else {
-
-        for(let i=0; i<parseInt(x); i++) {
+        for(let i = xStart; i<parseInt(xEnd); i++) {
             let img = imgs[i];
             const divImg = document.createElement('div');
             divImg.classList.add('image');
             const picture = document.createElement('img');
             picture.src = `https://farm${img.farm}.staticflickr.com/${img.server}/${img.id}_${img.secret}.jpg`;  
             divImg.appendChild(picture);
-            div.appendChild(divImg);
+            const link = document.createElement('a');
+            link.href = picture.src;
+            link.target = '_blank';
+            link.appendChild(divImg);
+            containerImg.appendChild(link);
+            nextStart = parseInt(xEnd);
+            console.log(nextStart);
             }
     }
-    containerImg.appendChild(div); 
+    containerImg.classList.add('images');
 }
 
-searchButton.addEventListener('click',downloadData);
-counts.forEach(count => count.addEventListener('click',numberOfImages));
+
